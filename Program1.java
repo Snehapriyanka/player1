bd65b45f978ae9272c05c5b134568351a93fc56b
Snehapriyanka <39979282+Snehapriyanka@users.noreply.github.com>
2018-08-09 11:33:41 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		String str,rev="";
+		Scanner c=new Scanner(System.in);
+		 str=c.nextLine();
+		int n=str.length();
+		for(int i=n-1;i>=0;i--) {
+		rev=rev+str.charAt(i);
+		}
+		System.out.println(""+rev);
+		}
+
+	}
+
+
